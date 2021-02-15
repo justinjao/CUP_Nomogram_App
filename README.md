88e80796e66bf710ad415e721f8c0e2068ddfcf0
justinjao <jao.c.justin@gmail.com>
2021-02-14 22:06:58 -0800

    added project files for completed app

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # Cancers of Unknown Primary Nomogram Application
-A nomogram app for Cancers of Unknown Primary (CUP)
+A nomogram app for Cancers of Unknown Primary (CUP). The link to the app can be found (here)[https://cupnomogram.shinyapps.io/Nomogram/].
 
-This is a shiny application demonstrating the usage of a nomogram for Cancers of Unknown Primary, for potential use in oncology clinics. 
+This is a shiny application demonstrating the usage of a nomogram for Cancers of Unknown Primary, for potential use in oncology clinics.
 
 ## The CUP Problem
 
+It is a well known problem within oncology that prognostication for cancers of unknown primary is significantly more uncertain, and assessments on survival rates can vary widely between different physicians.
+
+To simplify this problem, we have devised a nomogram to display estimated One Year & Two Year Overall Survival (OS) rates, using 5 well known and validated prognostic factors: Sex, Eastern Cooperative Oncology Group (ECOG) Status, Histology, Number of Metastatic Sites, and the Neutrophil-Lymphocite Ratio (NLR). The nomogram was devised by Raghav et al. To allow ease of real time use of the nomogram in clinical settings, we designed a simple web and mobile friendly application. The implementation and code used to design the app is provided here.
+
 ## About the App:
 
+The app was designed and deployed using the R Shiny package.
 
+The sidebar details the 5 prognostic factors that can be modified.
 
-### How to Use the App:
+The patient score is displayed in case the user may want to consult the original nomogram pictorial in the article.
+
+The 2 gauges displayed in center change reactively to user input, automatically calculating survival rates.
 
 
 ## Acknowledgments:
 
-More background information about CUP diagnosis, as well as information on how the nomogram was devised are detailed here: (insert link to journal article when published). 
+More background information about CUP diagnosis, as well as information on how the nomogram was created are detailed here: (manuscript is currently under review. The article link will be posted when it has been published).
---
+++ app.R
@@ -0,0 +1,147 @@
+# Script Comments ---------------------------------------------------------
+# Script for the Cancer of Unknown Primary Nomogram Shiny Application.
+# The app takes in input from the users and calculates a total score based on
+# the 5 factors, which is used to calculate 1 and 2 year survival.
+
+# Load Necessary Packages -------------------------------------------------
+
+library(shiny)
+library(shinyWidgets)
+library(flexdashboard)
+library(shinydashboard)
+
+#defining the CSS settings for the Gauge widget
+css <- HTML("
+.html-widget.gauge svg {
+  height: 200px;
+  width: 300px;
+  
+}")
+# Defining UI (Front-End) -------------------------------------------------
+
+# Define UI for application. This uses the dashboard layout with a black skin
+ui <-dashboardPage(skin = "blue", 
+                   
+                   # set the title
+                   dashboardHeader(title = "Cancers of Unknown Primary Nomogram", titleWidth = 450),
+                   
+                   # set the input widgets for the sidebar
+                   dashboardSidebar(width = 450,
+                                    radioButtons(inputId = "sex", label = "Sex",
+                                                 choices = c("Female" = "female",
+                                                             "Male" = "male")),
+                                    sliderTextInput(inputId = "ecog", label = "ECOG Status",
+                                                    choices = c("0", "1", "2", ">2"), selected = "0", grid = TRUE, width = 400),
+                                    radioButtons(inputId = "histology", label = "Histology",
+                                                 choices = c("Non-Adenocarcinoma" = "nonadeno",
+                                                             "Adenocarcinoma" = "adeno")),
+                                    radioButtons(inputId = "numsites", label = "Number of Metastatic Sites",
+                                                 choices = c("<3","≥3")),
+                                    radioButtons(inputId = "nlr", label = "Neutrophil-Lymphocite Ratio (NLR)",
+                                                 choices = c("<5", "≥5"))
+                                    
+                   ),
+                   
+                   # set the UI for the main panel 
+                   dashboardBody(
+                     #use the CSS styling defined above
+                     tags$head(tags$style(css)),
+                     # intro paragraph
+                     h4("Survival estimates can often be challenging for clinicians to provide when 
+ seeing patients with cancers of unknown primary.  Here we provide a simple application 
+ to estimate 1 and 2 year overall survival rates for patients with cancers of unknown 
+ primary, based on 5 well-known and validated prognostic factors."),
+                    
+                     #line breaks 
+                     br(), 
+                     br(),
+                     
+                     # use the fluidrows mechanism to structure outputs
+                     # width of screen is 12, so offset & width of 4 is perfectly center
+                     # sets UI for the value box and the 2 gauges
+                     fluidRow(
+                       column(width = 4, offset = 4, 
+                              valueBoxOutput("vbox", width = NULL),
+                      gaugeOutput("oneYrSurv", width = "100%", height = "100%"), 
+                     gaugeOutput("twoYrSurv", width = "100%", height = "100%") 
+                     )),
+                     
+                     # closing paragraph
+                     strong("Acknowledgements"),
+                     h4("The CUP App is constructed based on a novel nomogram 
+                     devised by a team of clinicians and researchers across three 
+                     institutions (the MD Anderson Cancer Center, BC Cancer, and 
+                     Sarah Cannon Cancer Center). More information about how the 
+                     factors were chosen, and performance of the nomogram can be 
+                     found here:"),
+                     
+                     br(),
+                    strong("(reference will be inserted when available)"),
+                    br(),
+                     
+                     # logos 
+                     img(src = "mdanderson.svg", height = 240, width = 240),
+                     img(src = "bccancer.png", height = 120, width = 120),
+                     img(src = "sarahcannon.svg", height = 300, width = 300)
+                   )
+)
+
+# Defining Server (Back-End) ----------------------------------------------
+
+# Define server logic 
+server <- function(input, output) {
+  
+  # sum the inputs from the user
+  # switch is used to change the value of each variable depending on user input
+  totalSum <- reactive({
+    sum(sex <- switch(input$sex,
+                      "female" = 0,
+                      "male" = 25),
+        ECOG <- switch(as.character(input$ecog),
+                       "0"  = 0,
+                       "1"  = 41,
+                       "2"  = 69,
+                       ">2" = 100),
+        histology <- switch(input$histology,
+                            "nonadeno" = 0,
+                            "adeno" = 27),
+        
+        numsites <- switch(input$numsites,
+                           "<3" = 0,
+                           "≥3" = 31),
+        nlr <- switch(input$nlr,
+                      "<5" = 0,
+                      "≥5" = 64)
+    )
+  })
+  
+  # render the value box based on totalSum
+  output$vbox <- renderValueBox({
+    valueBox(totalSum(), subtitle = "Patient Score", icon = icon("notes-medical"))
+  })
+  
+  #calculates the different survival rates and converts to a percentage
+  oneYrSurv <- reactive(exp(-exp(0.0123 * totalSum() - 1.95)) * 100)
+  twoYrSurv <- reactive(exp(-exp(0.0123 * totalSum() - 1.09)) * 100)
+  
+  # output gauge for one year survival. Rounds values and ensures minimum value is 10
+  output$oneYrSurv <- renderGauge({
+    gauge(min = 0, max = 100, 
+          value = round(ifelse(oneYrSurv() < 10,
+                               10,
+                               oneYrSurv())), 
+          symbol = "%", label = "One Year Survival")
+  })
+  
+  # output gauge for two year survival. Rounds values and ensures minimum value is 10
+  output$twoYrSurv <- renderGauge({
+    
+    gauge(min = 0, max = 100, value = round(ifelse(twoYrSurv() < 10,
+                                                   10,
+                                                   twoYrSurv())), 
+          symbol = "%", label = "Two Year Survival")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
